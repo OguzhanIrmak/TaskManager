8847dbe2e80238ee61687ab96850be5f48270bb2
OguzhanIrmak <oguzhanirmak01@gmail.com>
2024-08-28 01:21:09 +0300

    code edits

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +15,13 @@ namespace TaskTurner
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            CultureInfo culture=new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+        }
     }
 }
